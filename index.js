2223368c684d716b9439400a4260a8503fd65cf0
JonahPeck <PeckJonahC@gmail.com>
2023-01-31 16:13:53 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'joe';
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+bestCustomer = 'not bob';
+bestCustomer;
+}
+function overwriteBestCustomer() {
+   bestCustomer = 'maybe bob';
+    bestCustomer;
+}
+ function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bill";
+    leastFavoriteCustomer;
+ }
